4e2b861dea58a9dbb5cb2963cdee1a24351aed19
Justin Park <jutpark@ucsc.edu>
2024-04-06 18:03:47 -0700

    Adding new TFt stuff

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -1,34 +1,54 @@
 // project.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Justin Park
+// Date: 4/6/2024
 
 // NOTE: This is how we might start a basic JavaaScript OOP project
 
 // Constants - User-servicable parts
 // In a longer project I like to put these in a separate file
-
-// define a class
-class MyProjectClass {
-  // constructor function
-  constructor(param1, param2) {
-    // set properties using 'this' keyword
-    this.property1 = param1;
-    this.property2 = param2;
-  }
-  
-  // define a method
-  myMethod() {
-    // code to run when method is called
-  }
-}
-
 function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
+const fillers = {
+  Units: ["Azir", "Hwei", "Irelia", "Lissandra", "Rakan", "Sett", "Udyr", "Wukong", "Xayah", "Annie", "Ashe", "Galio", "kaisa", "Kayn", "Lee Sin", "Lilia", "Morgana", "Nautilus", "Ornn", "Sylas", "Syndra", "Alune", "Amumu", "Aphelios", "Bard", "Diana", "Illaoi", "Soraka", "Tahm kench", "Thresh", "Tristana", "Volibear", "Yone", "Zoe", "Aatrox", "Gnar", "janna", "Kindred", "Lux", "neeko", "Qiyana", "Riven", "Senna","Shen", "Teemo", "Yorick", "Zyra", "Ahri", "Caitlyn", "Cho'Gath", "Darius", "Garen", "Jax", "Kha'Zix", "Kobuko", "Kog'Maw", "Malphite", "Reksai", "Sivir", "yasuo" ],
+  Classes: ["Altruist", "Arcanist", "Artist", "behemoth", "Bruiser", "Duelist", "Exalted", "Great", "Invoker", "Lovers", "Reaper", "Sage","Sniper", "Spirit Walker", "Trickshot", "Warden" ],
+  Origin: ["DragonLord", "Dryad", "Fated","Fortune", "Ghostly", "Heavenly", "Inkshadow", "Mythic", "Porcelain", "StoryWeaver", "Umbral"],
+  
+};
 
-  // call a method on the instance
-  myInstance.myMethod();
+const template = `Hi! Heres ur TFT comp to play
+
+On your final board it must contain at least 1 $Units.
+It should also have the $Classes and the $Origin trait active.
+
+Good luck on top 4'ing with these! 
+`;
+
+
+// STUDENTS: You don't need to edit code below this line.
+
+const slotPattern = /\$(\w+)/;
+
+function replacer(match, name) {
+  let options = fillers[name];
+  if (options) {
+    return options[Math.floor(Math.random() * options.length)];
+  } else {
+    return `<UNKNOWN:${name}>`;
+  }
 }
 
-// let's get this party started - uncomment me
-//main();
+function generate() {
+  let story = template;
+  while (story.match(slotPattern)) {
+    story = story.replace(slotPattern, replacer);
+  }
+
+  /* global box */
+  box.innerText = story;
+}
+
+/* global clicker */
+clicker.onclick = generate;
+
+generate();
+}
+main();
