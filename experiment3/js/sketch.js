4103f3c4cdccbc7ac69c1bf50bc750d11d54e097
Justin Park <jutpark@ucsc.edu>
2024-04-23 18:34:50 -0700

    adding tileset and sketch

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -77,3 +77,198 @@ function draw() {
 function mousePressed() {
     // code to run when mouse is pressed
 }
+
+
+let seed = 0;
+let tilesetImage;
+let currentGrid = [];
+let numRows, numCols;
+
+function preload() {
+  tilesetImage = loadImage(
+    "C:\Users\Justin\Desktop\cmpm169\experiment3\js\tilesetP8.png"
+  );
+}
+
+function reseed() {
+  seed = (seed | 0) + 1109;
+  randomSeed(seed);
+  noiseSeed(seed);
+  select("#seedReport").html("seed " + seed);
+  regenerateGrid();
+}
+
+function regenerateGrid() {
+  select("#asciiBox").value(gridToString(generateGrid(numCols, numRows)));
+  reparseGrid();
+}
+
+function reparseGrid() {
+  currentGrid = stringToGrid(select("#asciiBox").value());
+}
+
+function gridToString(grid) {
+  let rows = [];
+  for (let i = 0; i < grid.length; i++) {
+    rows.push(grid[i].join(""));
+  }
+  return rows.join("\n");
+}
+
+function stringToGrid(str) {
+  let grid = [];
+  let lines = str.split("\n");
+  for (let i = 0; i < lines.length; i++) {
+    let row = [];
+    let chars = lines[i].split("");
+    for (let j = 0; j < chars.length; j++) {
+      row.push(chars[j]);
+    }
+    grid.push(row);
+  }
+  return grid;
+}
+
+function setup() {
+  numCols = select("#asciiBox").attribute("rows") | 0;
+  numRows = select("#asciiBox").attribute("cols") | 0;
+
+  createCanvas(16 * numCols, 16 * numRows).parent("canvasContainer");
+  select("canvas").elt.getContext("2d").imageSmoothingEnabled = false;
+
+  select("#reseedButton").mousePressed(reseed);
+  select("#asciiBox").input(reparseGrid);
+
+  reseed();
+}
+
+
+function draw() {
+  randomSeed(seed);
+  drawGrid(currentGrid);
+}
+
+function placeTile(i, j, ti, tj) {
+  image(tilesetImage, 16 * j, 16 * i, 16, 16, 8 * ti, 8 * tj, 8, 8);
+}
+
+
+function generateGrid(numCols, numRows) {
+  let grid = [];
+  for (let i = 0; i < numRows; i++) {
+    let row = [];
+    for (let j = 0; j < numCols; j++) {
+      let k = noise(i / 50, j / 30);
+      if (abs(k - 0.5) < 0.05) {
+        row.push("l");
+      } else {
+        let innerValue = noise(i / 10, j / 10);
+        if (innerValue > 0.5) {
+          row.push("_");
+        } else {
+          row.push(".");
+        }
+      }
+    }
+    grid.push(row);
+  }
+
+  return grid;
+}
+
+function drawGrid(grid) {
+
+  background(128);
+  const g = 10;
+  const t = millis() / 1000.0;
+  let f = 0;
+  let l = 0;
+  let r=0;
+  let c = 0;
+  let h = 0;
+  let b=0;
+
+  noStroke();
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      placeTile(i, j, (4 * pow(noise(t / 5, i, j / 2 + t), 3)) | 0.5, 13);
+
+      if (gridCheck(grid, i, j, "_")) {
+        placeTile(i, j, (4 * pow(random(), g)) | 0, 1);
+        if(random(1)>0.99){
+          f=i;
+          l=j;
+          r=1;
+        }
+      } else {
+        drawContext(grid, i, j, "l", 9, 3, true);
+      }
+
+      if (gridCheck(grid, i, j, ".")) {
+        placeTile(i, j, (4 * pow(random(), g)) | 0, 0);
+        if(random(1)>0.99){
+          c=i;
+          h=j;
+          b=1;
+        }
+      } else {
+        drawContext(grid, i, j, ".", 4, 0);
+      }
+    }
+  }
+  if(b==1){
+    placeTile(c, h, (1 * pow(random(), g)) | 26, 0);
+  }
+  if(r==1){
+    placeTile(f, l, (1 * pow(random(), g)) | 17, 0);
+  }
+}
+
+function drawContext(grid, i, j, target, dti, dtj, invert = false) {
+  let code = gridCode(grid, i, j, target);
+  if (invert) {
+    code = ~code & 0xf;
+  }
+  let [ti,tj] = lookup[code];
+  placeTile(i, j, dti + ti, dtj + tj);
+}
+
+
+
+function gridCode(grid, i, j, target) {
+  return (
+    (gridCheck(grid, i - 1, j, target) << 0) +
+    (gridCheck(grid, i, j - 1, target) << 1) +
+    (gridCheck(grid, i, j + 1, target) << 2) +
+    (gridCheck(grid, i + 1, j, target) << 3)
+  );
+}
+
+function gridCheck(grid, i, j, target) {
+  if (i >= 0 && i < grid.length && j >= 0 && j < grid[i].length) {
+    return grid[i][j] == target;
+  } else {
+    return false;
+  }
+}
+
+const lookup = [
+  [1, 1],
+  [1, 0], // bottom
+  [0, 1], // right
+  [0, 0], // right+bottom
+  [2, 1], // left
+  [2, 0], // left+bottom
+  [1, 1],
+  [1, 0], // * 
+  [1, 2], // top
+  [1, 1],
+  [0, 2], // right+top
+  [0, 1], // *
+  [2, 2], // top+left
+  [2, 1], // *
+  [1, 2], // *
+  [1, 1]
+];
+
+
